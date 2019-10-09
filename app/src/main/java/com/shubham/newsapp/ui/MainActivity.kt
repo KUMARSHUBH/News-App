5412ff347a5ecfb44a827f44f4db7698fa2fa840
Kumar Shubam <ks92217@gmail.com>
2019-10-09 15:52:42 +0530

    Basic splash screen

--- app/src/main/java/com/shubham/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/shubham/newsapp/ui/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setTheme(R.style.AppTheme)
         setSupportActionBar(toolbar)
         val fragPagerItems = FragmentPagerItems.with(this)
             .add(R.string.discover, Discover::class.java)
